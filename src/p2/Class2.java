ed6705ec98f6b5d71bd670b2be8f0a603b365bd3
Amruta M <amruta.markhedkar@gmail.com>
2025-07-20 12:20:29 +0530

    Branch A changes

--- src/p2/Class2.java
+++ src/p2/Class2.java
@@ -2,7 +2,8 @@ package p2;
 
 public class Class2 {
 	public static void main(String[] args) {
-		System.out.println("P2 c2 world");
+		System.out.println("P2 c2 world ----Scenario 2 ");
 		System.out.println("123 - stash 2");
+		System.out.println("Scenario 2");
 	}
 }
