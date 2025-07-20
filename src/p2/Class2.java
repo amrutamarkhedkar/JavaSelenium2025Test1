a5b5aa8f3a9170ae665eef73ad0cd41a87361de1
Amruta M <amruta.markhedkar@gmail.com>
2025-07-20 12:23:12 +0530

    Same file chnages from Branch B

--- src/p2/Class2.java
+++ src/p2/Class2.java
@@ -4,5 +4,6 @@ public class Class2 {
 	public static void main(String[] args) {
 		System.out.println("P2 c2 world");
 		System.out.println("123 - stash 2");
+		System.out.println("Branch B chnages for Scenario 2");
 	}
 }
